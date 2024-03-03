59e0b4370a67bd5a65b9536a22e9fcac84de2373
VN The Boss Baby <21520178@gm.uit.edu.vn>
2024-03-03 09:12:21 +0700

    init project

---
+++ lib/core/values/constants.dart
@@ -0,0 +1,21 @@
+class Constants {
+  static const KEY_STATUS_LOGIN = 'IS_LOGIN';
+  static const KEY_COLLECTION_ACCOUNT = 'ACCOUNT_COLLECTION';
+  static const KEY_COLLECTION_ROOM = 'ROOM_COLLECTION';
+  static const KEY_STORAGE_ACCOUNT_IMG = 'ACCOUNT_IMG';
+  static const API_GOOGLE_MAP = 'AIzaSyBC5XsEcx90TU2pWq2iTPCBN58dTKUmKSM';
+  static const KEY_COLLECTION_MESSAGE = 'MESSAGE_COLLECTION';
+  static const KEY_COLLECTION_ROOM_MESSAGE = 'ROOM_MESSAGE_COLLECTION';
+  static const KEY_COLLECTION_ROOM_USER = 'ROOM_USER_COLLECTION';
+  static const KEY_ACCOUNT_USERNAME = 'username';
+  static const KEY_ACCOUNT_UID = 'uid';
+  static const KEY_ACCOUNT_PHONENUMBER = 'phoneNumber';
+  static const KEY_ACCOUNT_EMAIL = 'email';
+  static const KEY_ACCOUNT_PHOTOURL = 'photoUrl';
+  static const KEY_ACCOUNT_SEX = 'sex';
+  static const KEY_ACCOUNT_DATEOFBIRTH = 'dateOfBirth';
+  static const KEY_ACCOUNT_ADDRESS = 'address';
+  static const KEY_ACCOUNT_AGE = 'age';
+  static const KEY_ACCOUNT_DATEOFCREATE = 'dateOfCreate';
+  static const KEY_ACCOUNT_ISONLINE = 'isOnline';
+}
