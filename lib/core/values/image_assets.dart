adf8d750aedd364ccece725d1e8fd38bec9759b6
nhubaole <okamisameko7123@gmail.com>
2024-03-16 15:55:36 +0700

    import assets

---
+++ lib/core/values/image_assets.dart
@@ -0,0 +1,37 @@
+abstract class ImageAssets {
+  static const arrowRight = 'images/svg/arrow-right.svg';
+  static const bannerVocab = 'images/svg/banner_vocab.svg';
+  static const icAdd2 = 'images/svg/ic_add_2.svg';
+  static const icAdd = 'images/svg/ic_add.svg';
+  static const icBack = 'images/svg/ic_back.svg';
+  static const icBook = 'images/svg/ic_book.svg';
+  static const icClose2 = 'images/svg/ic_close2.svg';
+  static const icClose = 'images/svg/ic_close.svg';
+  static const icEdit = 'images/svg/ic_edit.svg';
+  static const icFlashcard = 'images/svg/ic_flashcard.svg';
+  static const icFolder = 'images/svg/ic_folder.svg';
+  static const icGrammar = 'images/svg/ic_grammar.svg';
+  static const icHeadphone = 'images/svg/ic_headphones.svg';
+  static const icIdiom = 'images/svg/ic_idiom.svg';
+  static const icIrrVerb = 'images/svg/ic_irrverb.svg';
+  static const icPlus = 'images/svg/ic_plus.svg';
+  static const icSave = 'images/svg/ic_save.svg';
+  static const icSaved = 'images/svg/ic_saved.svg';
+  static const icSearch = 'images/svg/ic_search.svg';
+  static const icSpeaker = 'images/svg/ic_speaker.svg';
+  static const icTag = 'images/svg/ic_tag.svg';
+  static const icVideo = 'images/svg/ic_video.svg';
+  static const icWordset = 'images/svg/ic_wordset.svg';
+  static const icChevronDown = 'images/svg/ic-chevron-down.svg';
+  static const triangleRight = 'images/svg/triangle-right.svg';
+  static const icHome = 'images/svg/ic_home.svg';
+  static const icHomeFill = 'images/svg/ic_home_fill.svg';
+  static const icCommunity = 'images/svg/ic_community.svg';
+  static const icCommunityFill = 'images/svg/ic_community_fill.svg';
+  static const icEditFill = 'images/svg/ic_edit_fill.svg';
+  static const icGame = 'images/svg/ic_game.svg';
+  static const icGameFill = 'images/svg/ic_game_fill.svg';
+  static const icAccount = 'images/svg/ic_account.svg';
+  static const icAccountFill = 'images/svg/ic_account_fill.svg';
+  static const bgHome = 'images/png/bg_home.png';
+}
