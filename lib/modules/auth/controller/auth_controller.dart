7da0677fc25be2bbf8213dccd860875acf0e6719
VN The Boss Baby <21520178@gm.uit.edu.vn>
2024-06-22 00:33:55 +0700

    hot-fix: change model toMap()

--- lib/modules/auth/controller/auth_controller.dart
+++ lib/modules/auth/controller/auth_controller.dart
@@ -88,6 +88,7 @@ class AuthController extends GetxController {
         // role: Constants.ROLE_STUDENT,
         role: Role.ROLE_STUDENT.toStringValue,
       );
+      //print(userModel.toMap());
       var rs = await AuthServices.instance.addUserProfile(userModel: userModel);
       if (rs.isRight) {
         user = rs.right;
