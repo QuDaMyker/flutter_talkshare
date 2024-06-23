fe226d112bc40e918225551fe5393bd40423c64b
VN The Boss Baby <21520178@gm.uit.edu.vn>
2024-06-23 13:48:10 +0700

    hot-fix: show error message login, offall to rootview

--- lib/modules/root_view/view/root_view_screen.dart
+++ lib/modules/root_view/view/root_view_screen.dart
@@ -21,52 +21,41 @@ class RootViewScreen extends StatelessWidget {
         statusBarBrightness: Brightness.light,
         systemNavigationBarIconBrightness: Brightness.light,
       ),
-      child: Material(
-        color: Colors.transparent,
-        child: Scaffold(
-          body: PageView(
-            onPageChanged: (value) => rootViewController.animateToTab(value),
-            controller: rootViewController.pageController,
-            // physics: const NeverScrollableScrollPhysics(),
-            children: rootViewController.screens,
-          ),
-          bottomNavigationBar: Obx(
-            () => SalomonBottomBar(
-              currentIndex: rootViewController.currentPage.value,
-              onTap: (value) {
-                rootViewController.onChangePage(value);
-              },
-              items: [
-                SalomonBottomBarItem(
-                    icon: SvgPicture.asset(ImageAssets.icHome),
-                    title: const Text("Trang chủ"),
-                    selectedColor: AppColors.primary40,
-                    activeIcon: SvgPicture.asset(ImageAssets.icHomeFill)),
-                SalomonBottomBarItem(
-                    icon: SvgPicture.asset(ImageAssets.icCommunity),
-                    title: const Text("Cộng đồng"),
-                    selectedColor: AppColors.primary40,
-                    activeIcon: SvgPicture.asset(ImageAssets.icCommunityFill)),
-                // SalomonBottomBarItem(
-                //     icon: SvgPicture.asset(
-                //       ImageAssets.icEditThin,
-                //       color: AppColors.gray20,
-                //     ),
-                //     title: const Text("Bài tập"),
-                //     selectedColor: AppColors.primary40,
-                //     activeIcon: SvgPicture.asset(ImageAssets.icEditFill)),
-                SalomonBottomBarItem(
-                    icon: SvgPicture.asset(ImageAssets.icGame),
-                    title: const Text("Game"),
-                    selectedColor: AppColors.primary40,
-                    activeIcon: SvgPicture.asset(ImageAssets.icGameFill)),
-                SalomonBottomBarItem(
-                    icon: SvgPicture.asset(ImageAssets.icAccount),
-                    title: const Text("Tài khoản"),
-                    selectedColor: AppColors.primary40,
-                    activeIcon: SvgPicture.asset(ImageAssets.icAccountFill)),
-              ],
-            ),
+      child: Scaffold(
+        body: PageView(
+          onPageChanged: (value) => rootViewController.animateToTab(value),
+          controller: rootViewController.pageController,
+          // physics: const NeverScrollableScrollPhysics(),
+          children: rootViewController.screens,
+        ),
+        bottomNavigationBar: Obx(
+          () => SalomonBottomBar(
+            currentIndex: rootViewController.currentPage.value,
+            onTap: (value) {
+              rootViewController.onChangePage(value);
+            },
+            items: [
+              SalomonBottomBarItem(
+                  icon: SvgPicture.asset(ImageAssets.icHome),
+                  title: const Text("Trang chủ"),
+                  selectedColor: AppColors.primary40,
+                  activeIcon: SvgPicture.asset(ImageAssets.icHomeFill)),
+              SalomonBottomBarItem(
+                  icon: SvgPicture.asset(ImageAssets.icCommunity),
+                  title: const Text("Cộng đồng"),
+                  selectedColor: AppColors.primary40,
+                  activeIcon: SvgPicture.asset(ImageAssets.icCommunityFill)),
+              SalomonBottomBarItem(
+                  icon: SvgPicture.asset(ImageAssets.icGame),
+                  title: const Text("Game"),
+                  selectedColor: AppColors.primary40,
+                  activeIcon: SvgPicture.asset(ImageAssets.icGameFill)),
+              SalomonBottomBarItem(
+                  icon: SvgPicture.asset(ImageAssets.icAccount),
+                  title: const Text("Tài khoản"),
+                  selectedColor: AppColors.primary40,
+                  activeIcon: SvgPicture.asset(ImageAssets.icAccountFill)),
+            ],
           ),
         ),
       ),
